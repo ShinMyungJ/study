666446560cf29008db065ccb1baf40168d1ae6f0
Myungj <94836126+Myungj@users.noreply.github.com>
2021-11-24 19:49:04 +0900

    third commit

--- keras/keras06_train_test2.py
+++ keras/keras06_train_test2.py
@@ -12,7 +12,23 @@ x_test = x[-2:]
 y_train = y[:8]
 y_test = y[-2:]
 
-print(x_train)
-print(x_test)
-print(y_train)
-print(y_test)
+#2. 모델구성
+model = Sequential()
+model.add(Dense(10, input_dim=1))
+model.add(Dense(40))
+model.add(Dense(200))
+model.add(Dense(15))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss = 'mse', optimizer = 'adam' )
+model.fit(x_train, y_train, epochs=100, batch_size=1)
+
+#4. 평가, 예측
+loss = model.evaluate(x_test, y_test)
+print('loss : ', loss)
+result = model.predict([11])
+print('11의 예측값 : ', result)
+
+# loss :  8.230927051045e-11
+# 11의 예측값 :  [[11.000008]]
