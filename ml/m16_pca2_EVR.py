a4b16ecb9a8dd4799f1c29094a35deac7f49c3c1
Myungj <94836126+Myungj@users.noreply.github.com>
2022-02-04 17:40:43 +0900

    Add files via upload

---
+++ ml/m16_pca2_EVR.py
@@ -0,0 +1,62 @@
+import numpy as np
+from sklearn.datasets import load_boston, fetch_california_housing, load_breast_cancer
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+import warnings
+warnings.filterwarnings(action='ignore')
+
+#1. 데이터
+# datasets = load_boston()
+datasets = load_breast_cancer()
+# datasets = fetch_california_housing()
+x = datasets.data
+y = datasets.target
+print(x.shape)          # (506, 13) -> (20640, 8)
+
+pca = PCA(n_components=30)
+x = pca.fit_transform(x)
+print(x.shape)          # (506, 8)
+
+pca_EVR = pca.explained_variance_ratio_
+print(pca_EVR)
+print(sum(pca_EVR))
+
+cumsum = np.cumsum(pca_EVR)
+print(cumsum)
+
+import matplotlib.pyplot as plt
+plt.plot(cumsum)
+# plt.plot(pca_EVR)
+plt.grid()
+plt.show()
+'''
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size=0.8, random_state=66, shuffle=True
+)
+
+#2. 모델
+from xgboost import XGBRegressor, XGBClassifier
+model = XGBClassifier()
+
+#3. 훈련
+model.fit(x_train, y_train, eval_metric='error')
+
+#4. 평가, 예측
+results = model.score(x_test, y_test)
+print("결과 : ", results)
+
+# (569, 30)
+# 결과 :  0.9736842105263158
+
+# (569, 25)
+# 결과 :  0.9649122807017544
+
+# (506, 11)
+# 결과 :  0.891970250038245
+
+# (506, 12)
+# 결과 :  0.8874143051779056
+
+# (506, 13)
+# 결과 :  0.8996428829583528
+'''
