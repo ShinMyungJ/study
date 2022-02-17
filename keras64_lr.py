c90322df376c3e78299218e93b0bb7b55b9f52ab
Myungj <94836126+Myungj@users.noreply.github.com>
2022-02-17 18:25:26 +0900

    Add files via upload

---
+++ keras64_lr.py
@@ -0,0 +1,24 @@
+##################### 마음껏 튜닝 ㄱㄱ
+
+x = 10.0             # 임의로 바꿔도 되.
+y = 10.0             # 목표값
+w = 0.5             # 가중치 초기값
+lr = 0.1
+epochs = 400
+
+for i in range(epochs):
+    predict = x * w
+    loss = (predict - y) ** 2       # mse
+    
+    print("Loss : ", round(loss, 4), "\tPredict : ", round(predict, 4))
+    
+    up_predict = x * (w + lr)
+    up_loss = (y - up_predict) ** 2
+    
+    down_predict = x * (w - lr)
+    down_loss = (y - down_predict) ** 2
+    
+    if(up_loss > down_loss):
+        w = w - lr
+    else:
+        w = w + lr
