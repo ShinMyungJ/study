095d5a4de82b359786c78d232fee7697dfd31494
Myungj <94836126+Myungj@users.noreply.github.com>
2022-05-18 00:03:00 +0900

    Update main.py

--- detectron2/main.py
+++ detectron2/main.py
@@ -1,5 +1,7 @@
-from Detector_seg import *
+from Detector_all import *
 
 detector = Detector_seg(model_type="IS") #object detection : model_type="OD"
 
-detector.onImage("images/1.jpg")
+# detector.onImage("images/1.jpg")
+
+detector.onVideo("street.mp4")
